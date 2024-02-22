5e6858f30a9d453e86bbd02fbc8dbc668498b3c2
Shivanshu <ks7876555@gmail.com>
2024-02-22 22:05:42 +0530

    add : typing text and moving border button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+import { Spotlight } from "@/components/Spotlight";
+import { TypewriterEffectSmooth } from "@/components/typewriter-effect";
+import { Button } from "@/components/moving-border";
+import { cn } from "@/utils/cn";
+
 export default function Home() {
-    return <div className="min-h-screen bg-yellow-100">Next</div>;
+    const words = [
+        {
+            text: "Hii",
+        },
+        {
+            text: "I",
+        },
+        {
+            text: "am",
+        },
+        {
+            text: "Shivanshu",
+            className: "text-blue-500, dark:text-blue-500",
+        },
+    ];
+    return (
+        <div className="min-h-screen bg-black flex justify-center items-center">
+            <div className="">
+                <Spotlight className="absolute -top-10  -left-10" fill="blue" />
+            </div>
+            <div className="text-white">
+                <TypewriterEffectSmooth words={words} />
+                <div className="w-full justify-center flex pt-10">
+                    <Button
+                        className=" text-lg font-semibold font-mono"
+                        borderRadius="1rem"
+                    >
+                        Hire me
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
